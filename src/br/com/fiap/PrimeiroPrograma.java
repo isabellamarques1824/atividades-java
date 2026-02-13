3d75d02b7fe63cc3101e7d81590341b7d28325a9
Laboratório FIAP <labsfiap@fiap.com.br>
2026-02-13 11:39:58 -0300

    atividade

--- src/br/com/fiap/PrimeiroPrograma.java
+++ src/br/com/fiap/PrimeiroPrograma.java
@@ -3,5 +3,13 @@ package br.com.fiap;
 public class PrimeiroPrograma {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Explique Orientação à Objetos: ");
+        System.out.println("Explique o que é objeto: ");
+        System.out.println("Explique o que é classe: ");
+        System.out.println("Explique o que é abstração: ");
+        System.out.println("Explique o que é herança: ");
+        System.out.println("Explique o que é encapsulamento: ");
+        System.out.println("Explique o que é polimorfismo: ");
+        System.out.println("Quais os principais tipos de polimorfismo? Explique cada um deles: ");
     }
 }
